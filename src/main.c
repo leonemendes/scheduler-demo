1a3304f4fe15f1b1c7c7e4ac4e6562365bfe4a22
leonemendes <leonemendes23@gmail.com>
2017-10-02 23:32:53 -0300

    Programa funcionando.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,22 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, flag;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=0; i < n; i++) {
+    flag = 1; //flag que verifica se alguma tarefa eh executada
+    
+  	for(int l = 0; l <= 4; l++) {
+			if((i + 1) % a[l] == 0){	
+			  /* Imprime resultado da n-esima iteracao do scheduler */				
+				printf("%d", l);
+				flag = 0;
+			}
+  	}
+  	if(flag == 1) printf("-");
+  	printf("\n");
   }
 
   return 0;
